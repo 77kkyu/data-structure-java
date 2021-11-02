3ab7166b7973f369a255cbd043d8f2167c0aab4a
77kkyu <77kkyu@gmail.com>
2021-11-02 09:58:07 +0900

    feat singlyLinkedListMain

---
+++ src/com/company/linkedlist/singly/SinglyLinkedListMain.java
@@ -0,0 +1,35 @@
+package com.company.linkedlist.singly;
+
+public class SinglyLinkedListMain {
+	public static void main(String[] args) throws CloneNotSupportedException {
+
+		SinglyLinkedList<Integer> original = new SinglyLinkedList<>();
+		original.add(10);	// original에 10추가
+
+		SinglyLinkedList<Integer> copy = original;
+		SinglyLinkedList<Integer> clone = (SinglyLinkedList<Integer>) original.clone();
+
+		copy.add(20);	// copy에 20추가
+		clone.add(30);	// clone에 30추가
+
+		System.out.println("original list");
+		for(int i = 0; i < original.size(); i++) {
+			System.out.println("index " + i + " data = " + original.get(i));
+		}
+
+		System.out.println("\ncopy list");
+		for(int i = 0; i < copy.size(); i++) {
+			System.out.println("index " + i + " data = " + copy.get(i));
+		}
+
+		System.out.println("\nclone list");
+		for(int i = 0; i < clone.size(); i++) {
+			System.out.println("index " + i + " data = " + clone.get(i));
+		}
+
+		System.out.println("\noriginal list reference : " + original);
+		System.out.println("copy list reference : " + copy);
+		System.out.println("clone list reference : " + clone);
+
+	}
+}
