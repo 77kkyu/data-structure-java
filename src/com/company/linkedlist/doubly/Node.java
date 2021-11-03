a547146309d79872a56f1ce1de26a5bc75f03d71
77kkyu <77kkyu@gmail.com>
2021-11-03 17:34:43 +0900

    feat doubly linkedList node

---
+++ src/com/company/linkedlist/doubly/Node.java
@@ -0,0 +1,15 @@
+package com.company.linkedlist.doubly;
+
+public class Node<E> {
+
+	E data;
+	Node<E> next;
+	Node<E> prev;
+
+	public Node(E data) {
+		this.data = data;
+		this.prev = null;
+		this.next = null;
+	}
+
+}
