d98ef5921585f4a902db0685db84b7cfea7acf76
77kkyu <77kkyu@gmail.com>
2021-11-16 20:37:59 +0900

    feat hashtable item

---
+++ src/com/company/hashtable/Item.java
@@ -0,0 +1,13 @@
+package com.company.hashtable;
+
+public class Item {
+
+	public String key;
+	public Object value;
+
+	public Item(String key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+
+}
